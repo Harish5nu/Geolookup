84810c47a07ff3a31b60c99825c09b1a40c18430
Harish Saud <saudharish770@gmail.com>
2025-02-08 22:24:46 +0545

    Add files via upload

---
+++ ipgeolook.py
@@ -0,0 +1,18 @@
+import requests
+
+def lookup_ip(ip_address):
+    url = f"https://ipinfo.io/{ip_address}/json"  # API URL to get IP info
+    response = requests.get(url)
+    data = response.json()
+    
+    if response.status_code == 200:
+        print(f"IP Address: {data['ip']}")
+        print(f"Location: {data['city']}, {data['region']}, {data['country']}")
+        print(f"Org: {data['org']}")
+        print(f"Hostname: {data['hostname']}")
+    else:
+        print("Could not retrieve information for this IP address.")
+
+if __name__ == "__main__":
+    ip = input("Enter the IP address to lookup: ")
+    lookup_ip(ip)
